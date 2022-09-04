ac0a1106bdf62eb08feccb776505bf0639dec950
Andrey Yolkin <andreyyolkin@gmail.com>
2022-09-04 19:25:46 +0300

    init

---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+import { defineNuxtConfig } from "@nuxt/bridge"
+export default defineNuxtConfig({
+  bridge: {
+    vite: true
+  },
+  // modules: [
+  //   '@unocss/nuxt'
+  // ],
+  // unocss: {
+  //   theme: {
+  //     breakpoints: {
+  //       sm: '600px',
+  //       md: '960px',
+  //       lg: '1264px',
+  //       xl: '1904px'
+  //     }
+  //   }
+  // }
+})
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mt-20">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
